623938d5d4899d401a19d85b92948c5540bb9379
mibass <matthewbass@gmail.com>
2017-11-03 16:23:40 -0500

    Making script check to see if it's already running to avoid multiple processes.

--- runs.sh
+++ runs.sh
@@ -1,4 +1,12 @@
 #!/bin/env bash
+
+#check to see if script is already running
+if ps ax | grep $0 | grep -v $$ | grep bash | grep -v grep
+then
+    echo "The script is already running."
+    exit 1
+fi
+
 source /grid/fermiapp/products/uboone/setup_uboone.sh
 setup uboonecode v05_08_00_04 -q e9:prof
 
